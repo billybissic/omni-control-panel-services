05f28e0cd5c3fe329785731fa0ee5f13fd73886f
Billy Bissic <bissic.billy@gmail.com>
2018-05-06 23:49:43 -0500

    updating the script to exit on error and cleaning up the output messages

--- build-services.sh
+++ build-services.sh
@@ -4,41 +4,42 @@ buildAllServices()
    PARENT_DIRECTORY=`pwd` 
    DIRECTORY_PREFIX="com.omni.systems."
    DIRECTORIES=( `ls -d ${DIRECTORY_PREFIX}*` )
-   echo "Parent_Directory =" $PARENT_DIRECTORY
-   echo "Directory_Prefix =" $DIRECTORY_PREFIX
+   echo "[INFO] Parent Directory:" $PARENT_DIRECTORY
+   echo "[INFO] Directory Prefix:" $DIRECTORY_PREFIX
 
-   #echo "Directories To Process: "
+   #Directories To Process
    for i in "${DIRECTORIES[@]}"
    do
-      echo "Entering Into Directory: " $i
+      echo "[INFO] Entering Into Directory: " $i
       cd ${i}
    
-      echo "Performing Maven Clean: "
+      echo "[INFO] Performing Maven Clean: "
       MAVEN_RESULTS=`mvn -B clean`
    
       if [[ $MAVEN_RESULTS =~ (BUILD[[:space:]]SUCCESS) ]]; then
-         echo "Successfully Cleaned: " $i 
+         echo "[INFO] --Successfully Cleaned: " $i 
       fi
    
       if [[ $MAVEN_RESULTS =~ (BUILD[[:space:]]FAILURE) ]]; then
-         echo "Failed to Clean: " $i
+         echo "[INFO] --Failed to Clean: " $i
+         exit 1
       fi
 
-      echo "Performing Maven Build: "
+      echo "[INFO] Performing Maven Build: "
    
       MAVEN_BUILD_RESULTS=`mvn -B package`
  
       if [[ $MAVEN_BUILD_RESULTS =~ (BUILD[[:space:]]SUCCESS) ]]; then
-         echo "Successfully Built: " $i
+         echo "[INFO] --Successfully Built: " $i
       fi
    
       if [[ $MAVEN_BUILD_RESULTS =~ (BUILD[[:space:]]FAILURE) ]]; then
-         echo "Failed To Build: " $i
-      return 1
+         echo "[INFO] --Failed To Build: " $i
+         exit 1
       fi
 
       cd ..
-      #echo "Entering Into Directory:" `pwd`
+      #echo "[INFO] Entering Into Directory:" `pwd`
     done
 }
 
