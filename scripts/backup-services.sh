ffe481c3015548557424de16a3bb3c63e8816bc4
Billy Bissic <bissic.billy@gmail.com>
2018-12-28 17:16:01 -0600

    correcting pipeline and continuing back up services script

--- scripts/backup-services.sh
+++ scripts/backup-services.sh
@@ -2,11 +2,19 @@ FILE_SUFFIX=`date +%Y%m%d%H%M%S`
 FILE_PREFIX='OMNI-Services.Backup'
 FILE_TYPE="tar.gz"
 TOMCAT_ROOT_DIR="/opt/tomcat/"
+ROOT_BACKUP_DIR="${TOMCAT_ROOT_DIR}backups"
+NEW_BACK_DIR="${FILE_PREFIX}.${FILE_SUFFIX}"
+
 WEBAPP_DIR="${TOMCAT_ROOT_DIR}webapp"
 
 BACKUP_DIR="${FILE_PREFIX}.${FILE_SUFFIX}"
-mkdir $BACKUP_DIR
-cd
+cd $TOMCAT_ROOT_DIR
+mkdir $ROOT_BACKUP_DIR
+cd $ROOT_BACKUP_DIR
+mkdir
+cd $WEBAPP_DIR
+
+
 
 if [ $? -eq 1 ]
 then
