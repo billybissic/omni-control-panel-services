ba20b76da20d21392f4222759d49438c6582df77
unknown <bissic.billy@gmail.com>
2019-06-02 21:23:43 -0500

    merging local changes with master

---
+++ com.omni.systems.volunteers/src/main/java/application/Volunteers.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package application;
+
+/**
+ * @author Billy Bissic
+ *
+ */
+public class Volunteers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
